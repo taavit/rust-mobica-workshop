ab58b61212c498adcd87d02713d66c78b8c429d9
Dawid Królak <taavit@gmail.com>
2024-05-18 13:56:10 +0200

    Pico template

--- memory.x
+++ memory.x
@@ -1,8 +1,15 @@
-MEMORY
-{
-  /* NOTE K = KiBi = 1024 bytes */
-  FLASH (rx) : ORIGIN = 0x08000000, LENGTH = 512K
-  RAM (rwx) : ORIGIN = 0x20000000, LENGTH = 128K
+MEMORY {
+    BOOT2 : ORIGIN = 0x10000000, LENGTH = 0x100
+    FLASH : ORIGIN = 0x10000100, LENGTH = 2048K - 0x100
+    RAM   : ORIGIN = 0x20000000, LENGTH = 256K
 }
 
-_stack_start = ORIGIN(RAM) + LENGTH(RAM);
+EXTERN(BOOT2_FIRMWARE)
+
+SECTIONS {
+    /* ### Boot loader */
+    .boot2 ORIGIN(BOOT2) :
+    {
+        KEEP(*(.boot2));
+    } > BOOT2
+} INSERT BEFORE .text;
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,58 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
+use bsp::hal::clocks::init_clocks_and_plls;
+use bsp::hal::Sio;
+use bsp::hal::Watchdog;
 use defmt::debug;
 use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::OutputPin;
 use panic_probe as _;
 
+use bsp::hal as rp2040_hal;
 use defmt_rtt as _;
-use stm32f4xx_hal::{gpio::GpioExt, pac, prelude::*, rcc::RccExt, timer::SysTimerExt};
+use rp_pico as bsp;
+
+use bsp::entry;
+use bsp::pac;
 
 #[entry]
 fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
-    let cp = cortex_m::peripheral::Peripherals::take().unwrap();
+    let mut pac = pac::Peripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
 
-    let gpioa = dp.GPIOA.split();
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
 
-    let rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
-    let mut delay = cp.SYST.delay(&clocks);
+    // let delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut delay = rp2040_hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+    let pins = bsp::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
 
-    let mut led = gpioa.pa5.into_push_pull_output();
+    let mut led = pins.gpio16.into_push_pull_output();
+    // let button = pins.gpio18.into_pull_down_input();
 
     loop {
-        led.set_high();
+        led.set_high().unwrap();
         debug!("Set high");
         delay.delay_ms(250);
-        led.set_low();
+        led.set_low().unwrap();
         debug!("Set low");
         delay.delay_ms(250);
     }
